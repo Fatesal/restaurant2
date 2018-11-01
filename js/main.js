4eed9246f89ed4c962429d20e8aa074375aa768e
Fatesal <39712588+Fatesal@users.noreply.github.com>
2018-11-01 20:27:11 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -80,7 +80,7 @@ initMap = () => {
         scrollWheelZoom: false
     });
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1IjoiYWxleGFuZHJpIiwiYSI6ImNqaW15aGtjczA5Y2EzcG56MG5xcnFvaW0ifQ.Gg23g58sb7PpKdVsAZaWgQ',
+        mapboxToken: 'pk.eyJ1IjoiZmFpdGhzYWx6IiwiYSI6ImNqbXQzZjYwaDAycmszbHA3OWJrMDZ6bHgifQ.r5BhHeGSD3ot-qv-2ZkBUg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
             '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
